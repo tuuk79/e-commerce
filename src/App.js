6057c6ad3a44f7906ad6c7c611296a25ef0674f9
tuuk79 <stevecyoo@gmail.com>
2019-06-16 12:51:10 -0500

    added routing to product-add page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import ProductList from './product/product-list';
+import ProductAdd from './product/product-add';
 import UserList from './user/user-list';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
@@ -14,12 +15,16 @@ function App() {
 							<Link to="/product-list">Products</Link>
 						</li>
 						<li>
+							<Link to="/product-add">Add Product</Link>
+						</li>
+						<li>
 							<Link to="/user-list">Users</Link>
 						</li>
 					</ul>
 				</nav>
 
 				<Route path="/product-list" exact component={ProductList} />
+				<Route path="/product-add" exact component={ProductAdd} />
 				<Route path="/user-list" exact component={UserList} />
 			</div>
 		</Router>
