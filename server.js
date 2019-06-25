6a4088269cf81c9dabd99d76fc95bf9c3bcb6394
steve.yoo <stevecyoo@gmail.com>
2019-06-25 17:16:16 -0500

    updated server.js to serve app using node instead of create-react-app

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(path.join(__dirname, 'build')));
+
+ app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
