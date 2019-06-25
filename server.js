23b9d3ccee2d0166890de6eaf54e94762efa5ef7
steve.yoo <steve.yoo@servicenow.com>
2019-06-25 17:13:25 -0500

    Revert "updating path to index.html start up page to be served from express app"
    This reverts commit 01d69c428a03b41f31131cbc231b783cdcee1238.

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
